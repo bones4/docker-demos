e194f528cb27892a520b9ed6dfbccd6e40b7ceb4
bones4 <1438008173@qq.com>
2019-07-18 14:39:47 +0800

    Update Application.java

--- samples/spring-boot/src/main/java/com/waylau/docker_spring_boot/Application.java
+++ samples/spring-boot/src/main/java/com/waylau/docker_spring_boot/Application.java
@@ -16,7 +16,7 @@ public class Application {
 
 	@RequestMapping("/")
 	public String home() {
-		return "Hello Docker World.v1.6. <br />"
+		return "Hello Docker World.修改为2.39. <br />"
 				+ "Welcome to <a href='http://waylau.com'>waylau.com</a></li>";
 	}
 
